80eae51ae0eb6d11ae2644c9ec7a937cdd62791b
alexCT <alexCT@DESKTOP-MT41G95>
2021-02-05 13:03:19 -0500

    Final

--- ExamenProveedor2JEE/src/main/java/ec/edu/ups/appdis/examen/modelo/Productos.java
+++ ExamenProveedor2JEE/src/main/java/ec/edu/ups/appdis/examen/modelo/Productos.java
@@ -10,7 +10,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "ferr_producto")
+@Table(name = "ferr_producto" )
 public class Productos implements Serializable {
 
 	/**
